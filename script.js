4f05b1b31127a9aa6d48f507977d357f38028600
Bajcon <45372842+Bajcon@users.noreply.github.com>
2023-02-14 14:51:01 +0100

    Update

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ const departureDate = document.querySelector('.departureDate');
 const destination = document.querySelector(".destinationChoice");
 const returnDate = document.querySelector(".destinationDate");
 const returnDateText = document.querySelector('.return-date-text');
+const passengers = document.querySelector('.numberOfPassengers');
+const passengerClass = document.querySelector('.ticketChoseClass')
 
 const paragraph = document.querySelector('.showDeparture')
 const paragraph2 = document.querySelector('.showDestination')
 const paragraph3 = document.querySelector('.showDepartureDate')
 const paragraph4 = document.querySelector('.showReturnDate')
+const paragraph5 = document.querySelector('.tripDuration')
+const paragraph6 = document.querySelector('.showPassengers')
+const paragraph7 = document.querySelector('.showClass')
 
 
 
@@ -21,10 +26,15 @@ function showWindow() {
         alert("You have not chosen any departure date")
         return
     }
+    if(departure.value === destination.value) {
+        alert ("You can't go where you are!")
+        return
+    }
     if(!returnDate.classList.contains('hidden') && returnDate.value === "") {
         alert("You have not chosen any return date")
         return   
     }
+    
     if(returnDate.value === "") {
         paragraph4.classList.add('hidden')
     } else {
@@ -36,12 +46,17 @@ function showWindow() {
         return
     }
 
+
     dropdown.classList.remove('hidden')
     console.log(destination.value)
     paragraph.innerText = "From: " + departure.value;
     paragraph2.innerText= "To: " + destination.value;
     paragraph3.innerText = "Departure date: " + departureDate.value;
     paragraph4.innerText = "Return date: " + returnDate.value;
+    paragraph5.innerText = "Trip duration: " + ((new Date(returnDate.value) - new Date(departureDate.value)) /1000 /24 /60 /60) + " days"
+    paragraph6.innerText = "Number of passengers: " + passengers.value;
+    paragraph7.innerText = "Travel class: " + passengerClass.value;
+
 
     //Verification
    
