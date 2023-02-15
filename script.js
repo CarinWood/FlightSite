0169f82561bce1c6c013be12d347558ed05f0bbf
fregget <fbjuren@gmail.com>
2023-02-15 11:57:15 +0100

    Price update

--- script.js
+++ script.js
@@ -14,8 +14,14 @@ const paragraph4 = document.querySelector(".showReturnDate");
 const paragraph5 = document.querySelector(".tripDuration");
 const paragraph6 = document.querySelector(".showPassengers");
 const paragraph7 = document.querySelector(".showClass");
+const paragraph8 = document.querySelector(".showPrice");
+
+var price;
 
 function showWindow() {
+  if (passengers.value === "") {
+    passengers.value = 1;
+  }
   if (departure.value === "" || destination.value === "") {
     alert("You have not chosen destination or depature");
     return;
@@ -34,17 +40,21 @@ function showWindow() {
 
   if (returnDate.value === "") {
     paragraph4.classList.add("hidden");
+    paragraph5.classList.add("hidden");
   } else {
     paragraph4.classList.remove("hidden");
+    paragraph5.classList.remove("hidden");
   }
 
-  if (departureDate.value > returnDate.value) {
+  if (departureDate.value > returnDate.value && returnDate.value != "") {
     alert("Return date can't be before departure date");
     return;
   }
 
   dropdown.classList.remove("hidden");
   dropdown.classList.add("slide-from-right");
+
+  //flytta till array och välj vad som skrivs ut för varje loop?
   paragraph.innerText = "From: " + departure.value;
   paragraph2.innerText = "To: " + destination.value;
   paragraph3.innerText = "Departure date: " + departureDate.value;
@@ -59,6 +69,8 @@ function showWindow() {
     " days";
   paragraph6.innerText = "Number of passengers: " + passengers.value;
   paragraph7.innerText = "Travel class: " + passengerClass.value;
+  createPrice();
+  paragraph8.innerText = "Ticet Price: " + "$" + price;
 
   //Verification
 }
@@ -67,6 +79,7 @@ function chooseOneWay() {
   returnDate.classList.add("hidden");
   returnDateText.classList.add("hidden");
   paragraph4.classList.add("hidden");
+  paragraph5.classList.add("hidden");
 }
 
 function chooseReturn() {
@@ -77,6 +90,20 @@ function chooseReturn() {
   returnDateText.classList.remove("hidden");
 }
 
+function createPrice() {
+  price = Math.floor(Math.random() * (500 - 200 + 1) + 200 * passengers.value);
+  if (passengerClass.value === "Business Class") {
+    price *= 2;
+  }
+  if (passengerClass.value === "First Class") {
+    price *= 3;
+  }
+}
+
 function payFunc() {
-    alert('246633$ has successfully been withdrawn from your bank account. \nThank you for your purchase!')
+  alert(
+    "$" +
+      price +
+      " has successfully been withdrawn from your bank account. \nThank you for your purchase!"
+  );
 }
